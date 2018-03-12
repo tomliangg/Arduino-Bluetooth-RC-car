0318aff68245f3ff38354e1b8db0788dabefc5d6
Tom Liang <tomliangg@users.noreply.github.com>
2018-03-12 14:20:04 -0700

    Add files via upload

---
+++ BT_Master.ino
@@ -0,0 +1,79 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(10, 11);    //Pin 10 -> TX on BT, Pin 11 -> RX on BT
+
+int state = 0;
+const byte button1 = 9;  //the pin your button is connected to pin 9 on Arduino
+const byte button2 = 8;  //if this button pressed, go forward
+const byte button3 = 7;  //if pressed, go forward right
+const byte button4 = 4;  //if pressed, go backward left
+const byte button5 = 3;  //if pressed, go backward
+const byte button6 = 2;  //if pressed, go backward right
+
+
+byte button1State = 0;
+byte button2State = 0;
+byte button3State = 0;
+byte button4State = 0;
+byte button5State = 0;
+byte button6State = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  BTserial.begin(38400);
+
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
+  pinMode(button5, INPUT);
+  pinMode(button6, INPUT);
+
+}
+
+void loop() {
+
+  button1State = digitalRead(button1);
+  button2State = digitalRead(button2);
+  button3State = digitalRead(button3);
+  button4State = digitalRead(button4);
+  button5State = digitalRead(button5);
+  button6State = digitalRead(button6);
+
+  
+  if (button1State == HIGH) {
+    BTserial.write('Q');  //sends a 1 through the bluetooth serial link
+    delay (20);
+  }
+
+  else if (button2State == HIGH){
+    BTserial.write('W');
+    delay(20);
+  }
+
+  else if (button3State == HIGH){
+    BTserial.write('E');
+    delay(20);
+  }
+
+  else if (button4State == HIGH){
+    BTserial.write('A');
+    delay(20);
+  }
+
+  else if (button5State == HIGH){
+    BTserial.write('S');
+    delay(20);
+  }
+
+  else if (button6State == HIGH){
+    BTserial.write('D');
+    delay(20);
+  }
+
+  else {
+    BTserial.write('P');
+    delay(20);
+  }
+  
+}
---
+++ BT_Slave.ino
@@ -0,0 +1,75 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(10, 11);    //Pin 10 -> TX on BT, Pin 11 -> RX on BT
+
+//L293 Connection   
+  const int motorA1  = 3;  // Pin  2 of L293D
+  const int motorA2  = 5;  // Pin  7 of L293D
+  const int motorB1  = 6; // Pin 10 of L293D
+  const int motorB2  = 9;  // Pin 15 of L293D
+
+int state = 0;
+
+void setup() {
+  // initialize digital pin 8 as an output.
+
+  Serial.begin(9600);
+  BTserial.begin(38400);
+
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2, OUTPUT);
+  pinMode(motorB1, OUTPUT);
+  pinMode(motorB2, OUTPUT);
+
+}
+
+void loop() {
+
+  if (BTserial.available() > 0) { // Checks whether data is comming from the serial port
+    state = BTserial.read(); // Reads the data from the serial port
+  }
+  
+    /***********************Forward****************************/
+  //If state is equal with letter 'W', car will go forward!
+    if (state == 'W') {
+      analogWrite(motorA1, 255); analogWrite(motorA2, 0);
+      analogWrite(motorB1, 255); analogWrite(motorB2, 0); 
+    }
+  /**********************Forward Left************************/
+  //If state is equal with letter 'Q', car will go forward left
+    else if (state == 'Q') {
+      analogWrite(motorA1, 20); analogWrite(motorA2, 0);  
+      analogWrite(motorB1, 200); analogWrite(motorB2, 0); 
+    }
+  /**********************Forward Right************************/
+  //If state is equal with letter 'E', car will go forward right
+    else if (state == 'E') {
+        analogWrite(motorA1, 200); analogWrite(motorA2, 0); 
+        analogWrite(motorB1, 20);  analogWrite(motorB2, 0); 
+    }
+  /***********************Backward****************************/
+  //If state is equal with letter 'S', car will go backward
+    else if (state == 'S') {
+      analogWrite(motorA1, 0);   analogWrite(motorA2, 255); 
+      analogWrite(motorB1, 0);   analogWrite(motorB2, 255); 
+    }
+  /**********************Backward Left************************/
+  //If state is equal with letter 'A', car will go backward left
+    else if (state == 'A') {
+      analogWrite(motorA1, 0); analogWrite(motorA2, 20); 
+      analogWrite(motorB1, 0); analogWrite(motorB2, 200); 
+    }
+  /**********************Backward Right************************/
+  //If state is equal with letter 'D', car will go backward right
+    else if (state == 'D') {
+      analogWrite(motorA1, 0);   analogWrite(motorA2, 200); 
+      analogWrite(motorB1, 0);   analogWrite(motorB2, 20); 
+    }
+
+      /************************Stop/Pause*****************************/
+  //If state is equal with letter 'P', pause/stop the car
+    else if (state == 'P'){
+        analogWrite(motorA1, 0);  analogWrite(motorA2, 0); 
+        analogWrite(motorB1, 0);  analogWrite(motorB2, 0);
+    }
+
+}
